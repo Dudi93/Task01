322c26c0fd243c8be00b823e09e4fe4f2a9a6c60
Dudi93 <kamilek93@gmail.com>
2015-08-06 11:52:19 +0200

    execl

--- main.c
+++ main.c
@@ -10,23 +10,13 @@ int main()
 	pthread_t thread1 = 0;
 	pthread_create(&thread1, 0, pthread_function, 0);
 	pthread_join(thread1, 0);
+	printf("Nie");
 	return 0;
 }
 
 void *pthread_function (void* ptr)
 {
-	char *argv[] = { "/bin/sh", "-c", "env", 0 };
-	char *envp[] =
-	{
-			"HOME=/",
-	        "PATH=/bin:/usr/bin",
-	        "TZ=UTC0",
-	        "USER=fvdsf",
-	        "LOGNAME=fdsfds",
-	        0
-	};
-	execve(argv[0], &argv[0], envp);
-	//while(1)
-	//	sleep(1);
+	execl( "/bin/ls", "ls", "-l", (char*)0 );
+	printf("No");
 	return 0;
 }
